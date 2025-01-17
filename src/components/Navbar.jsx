8a8d10926e5954315fa08b05cc46f85b48e59af4
Camomilte <camomilte@gmail.com>
2025-01-17 15:17:22 +0100

    Building up the basic pages, components and layouts, no styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>This is the NAVBAR</p>
+    </div>
+  )
+}
+
+export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
-import { BrowserRouter } from 'react-router'
+
+import RootLayout from './layouts/RootLayout'
+
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Product from './Pages/Product'
+import Contact from './Pages/Contact'
+import Login from './Pages/Login'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       
       <Routes>
-        <Route />
+        <Route element={ <RootLayout /> }>
+
+          <Route path='/' element={ <Home /> } />
+          <Route path='/shop' element={ <Shop /> } />
+          <Route path='/product-detail' element={ <Product /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='/login' element={ <Login /> } />
+
+        </Route>
+
       </Routes>
+
     </BrowserRouter>
   </StrictMode>,
 )
