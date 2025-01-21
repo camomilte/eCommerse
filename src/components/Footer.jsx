e6a016e411117fb25cc001db3b8b720d3135bd3c
Camomilte <camomilte@gmail.com>
2025-01-21 20:26:38 +0100

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+
+import logo from '../assets/icon.svg'
 
 const Footer = () => {
   return (
-    <div>
-      <p>This is the FOOTER</p>
-    </div>
+    <>
+    <Navbar className="bg-dark p-4" fixed="bottom">
+      <Container>
+        <Navbar.Brand href="#home" className='text-light'>
+          <img
+            src={logo}
+            height="30"
+            className="d-inline-block align-top"
+            alt="logo"
+          />The Bit Store</Navbar.Brand>
+
+        <Nav className="align-right">
+            <Nav.Link href="https://www.instagram.com/" alt="Instagram">
+              <i className="fa-brands fa-instagram text-white"></i>    
+            </Nav.Link>
+            <Nav.Link href="https://www.facebook.com/" alt="Facebook">
+              <i className="fa-brands fa-facebook text-white"></i>
+            </Nav.Link>
+            <Nav.Link href="https://www.youtube.com" alt="Youtube">
+              <i className="fa-brands fa-youtube text-white"></i>
+            </Nav.Link>
+            <Nav.Link href="https://www.linkedin.com" alt="Linkedin">
+              <i className="fa-brands fa-linkedin text-white"></i>
+            </Nav.Link>
+            <Nav.Link href="https://ww.tiktok.com" alt="Tiktok">
+              <i className="fa-brands fa-tiktok text-white"></i>
+            </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navigation = () => {
             />
             The Bit Store
           </Navbar.Brand>
-          <Nav className="me-auto mx-auto">
+          <Nav className="justify-content-center">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/shop">Shop</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
